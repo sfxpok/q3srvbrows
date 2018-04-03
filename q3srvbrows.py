a558f18811d18322fff79e7cf6ac670732a8a235
sfxpok <sfxpok@users.noreply.github.com>
2018-04-03 04:01:16 +0100

    trying to call quake from the terminal

--- q3srvbrows.py
+++ q3srvbrows.py
@@ -1,19 +1,24 @@
 import sys
 import os
 import requests
+import subprocess
 
-serverList=["195.133.146.111:20018","server.unfreeze.ga:27960"]
+serverList = ["195.133.146.111:20018","server.unfreeze.ga:27960"]
+
 
 def getDirectory():
 
-	if pathDirectory == NULL:
+	pathDirectory = None
+
+	if pathDirectory is None:
 		pathDirectory = raw_input("Enter the path of your file: ")
+		# executableName = raw_input("Enter the executable name: ")
 	else:
 		return
 
-	#assert os.path.exists(pathDirectory), "I did not find the file at, "+str(user_input)
-	#f = open(pathDirectory,'r+')
-	#print("Hooray we found your file!")
+	assert os.path.exists(pathDirectory), "I did not find the file at, "+str(pathDirectory)
+	f = open(pathDirectory,'r+')
+	print("Hooray we found your file!")
 	#stuff you do with the file goes here
 	#f.close()
 
@@ -28,12 +33,19 @@ def callServers():
 	else:
   		print hostname, 'is down!'
 
-def callServers_2():
+def pingServers_2():
 
 	apiResponse = requests.get("https://use.gameapis.net/quake3/query/info/195.133.146.111:20018")
 	print(apiResponse.content)
+	
+	# os.system('q3 195.133.146.111:20018')
+	subprocess.call('q3', shell=True, executable='/bin/bash')
+	
+# def printServers():
 
 	
 
-print callServers_2()
+	
+# print getDirectory()
+print pingServers_2()
 
